cf07b00f5af6c993ed36b5351d42b54682b2b1b9
egyMaret <egymaret@gmail.com>
2024-07-25 19:17:49 +0700

    update1

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-import tensorflow as tf
 from datetime import datetime
 from tensorflow.keras.models import load_model
 import pickle
